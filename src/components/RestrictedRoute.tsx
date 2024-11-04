335a3c5e343ffa2ce007a764cd21a478db2271dc
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-04 16:04:58 +0200

    add

---
+++ src/components/RestrictedRoute.tsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { selectIsAuthenticated } from "../redux/auth/selectors";
+import { selectIsLoading } from "../redux/pet/selectors";
+
+interface IProps {
+  component: React.ComponentType;
+  redirectTo?: string;
+}
+
+const RestrictedRoute: React.FC<IProps> = ({
+  component: Component,
+  redirectTo = "/",
+}) => {
+  const authenticated = useSelector(selectIsAuthenticated);
+
+  const isloading = useSelector(selectIsLoading);
+
+  if (isloading) return null;
+
+  return authenticated ? <Navigate to={redirectTo} replace /> : <Component/>;
+};
+
+export default RestrictedRoute;
+
