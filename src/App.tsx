27a9493ba54de90bd843190af0532f9e908235d4
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-04 16:02:47 +0200

    fix App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { SharedLayout } from "./components/SharedLayout";
+// import { lazy } from "react";
+
+import RestrictedRoute from "./components/RestrictedRoute";
+// import PrivateRoute from "./components/PrivateRoute";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+
+// const AllProductsPage = lazy(() => import("./pages/AllProductsPage"));
+// const DashboardPage = lazy(() => import("./pages/DashboardPage/DashboardPage"));
+// const AllOrdersPage = lazy(() => import("./pages/AllOrdersPage"));
+// const AllSuppliersPage = lazy(() => import("./pages/AllSuppliersPage"));
+// const CustomersDataPage = lazy(() => import("./pages/CustomersDataPage"));
 
 function App() {
-
   return (
     <>
-        Click on the Vite and React logos to learn more
+      <Routes>
+        <Route index element={<Navigate to="/home" />}></Route>
+        <Route
+          path="home"
+          element={
+            <RestrictedRoute component={HomePage} redirectTo="/register" />
+          }
+        />
+
+        <Route
+          path="register"
+          element={
+            <RestrictedRoute component={RegisterPage} redirectTo="/login" />
+          }
+        ></Route>
+
+        <Route
+          path="login"
+          element={<RestrictedRoute component={LoginPage} redirectTo="/home" />}
+        ></Route>
+
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="*" element={<Navigate to="home" />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
