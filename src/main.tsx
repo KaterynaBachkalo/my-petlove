95fc1c81a0edc4d28ebb50941a0458f87cc2aca8
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-04 16:04:00 +0200

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './scss/index.scss';
+import App from './App.tsx';
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.ts";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="/my-petlove">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </>,
 )
