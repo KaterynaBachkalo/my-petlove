53ee71e129e3d6f8b7e485c60c164be721850e89
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-04 16:04:50 +0200

    add

---
+++ src/components/Icon.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import sprite from "../img/sprite.svg";
+
+interface IProps {
+  className: string;
+  name: string;
+  width?: number;
+  height?: number;
+}
+
+const Icon: FC<IProps> = ({ name, width, height, className }) => (
+  <svg width={width} height={height} className={className}>
+    <use xlinkHref={`${sprite}#${name}`} />
+  </svg>
+);
+
+export default Icon;
