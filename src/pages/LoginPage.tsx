e46d7d4757f7296c7a11006a27ac7a5d60da850e
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-12 20:42:45 +0200

    fix

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,11 +5,44 @@ const LoginPage = () => {
   return (
     <>
       <div>
-        <header>
-          <nav></nav>
-        </header>
-        <div>
-          <FormLogin />
+        <picture>
+          <source
+            media="(min-width: 1280px)"
+            srcSet="src/img/authorization/login/desk/image@1x.png 1x, src/img/authorization/registration/desk/image@2x.png 2x"
+            type="image/png"
+            width={592}
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet="src/img/authorization/login/tab/image@1x.png 1x, src/img/authorization/registration/tab/image@2x.png 2x"
+            type="image/png"
+            width={704}
+          />
+          <source
+            media="(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)"
+            srcSet="src/img/authorization/login/image@1x.png 1x, src/img/authorization/registration/image@2x.png 2x"
+            type="image/png"
+          />
+          <img
+            src="src/img/authorization/login/image@1x.png"
+            alt="cat"
+            className="registration-img"
+            width="335"
+            height="280"
+          />
+        </picture>
+        <div className="login-wrap">
+          <h2 className="login-title">Log in</h2>
+          <h3 className="login-subtitle">
+            Welcome! Please enter your credentials to login to the platform:{" "}
+          </h3>
+          <FormLogin
+            formText="Don’t have an account?"
+            link="/register"
+            textLink="Register"
+            buttonText="Log in"
+            isInput={false}
+          />
         </div>
       </div>
 
