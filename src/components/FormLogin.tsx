f37e5f37c705397c2107f0cc0c11205a6f9310c7
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-12 20:41:26 +0200

    add

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -1,22 +1,42 @@
-import { useState } from "react";
+import { FC, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Icon from "./Icon";
 import { useDispatch } from "react-redux";
-import { logInThunk } from "../redux/auth/operations";
+import { logInThunk, registerThunk } from "../redux/auth/operations";
 import { AppDispatch } from "../redux/store";
-import { IForms } from "../types";
+import { IForms, IFormsBD } from "../types";
+import { Link } from "react-router-dom";
 
-const FormLogin = () => {
+interface IFormText {
+  formText: string;
+  link: string;
+  textLink: string;
+  buttonText: string;
+  isInput: boolean;
+}
+
+const FormLogin: FC<IFormText> = ({
+  formText,
+  link,
+  textLink,
+  buttonText,
+  isInput,
+}) => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const dispatch = useDispatch<AppDispatch>();
 
   const schema = yup
     .object({
+      name: yup.string(),
       email: yup.string().email().required(),
       password: yup.string().required().min(6),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), undefined], "Passwords must match"),
     })
     .required();
 
@@ -30,35 +50,89 @@ const FormLogin = () => {
   });
 
   const onSubmit = (data: IForms) => {
-    dispatch(logInThunk(data));
+    if (buttonText === "Registration") {
+      const dataBD: IFormsBD = {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      };
+
+      dispatch(registerThunk(dataBD));
+    } else {
+      const dataBD: IFormsBD = {
+        email: data.email,
+        password: data.password,
+      };
+      dispatch(logInThunk(dataBD));
+    }
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <input {...register("email")} placeholder="Email address" />
-        <p>{errors.email?.message}</p>
+      <div className="wrap-input">
+        {isInput && (
+          <div>
+            <input {...register("name")} placeholder="Name" className="input" />
+            <p className="form-errors">{errors.name?.message}</p>
+          </div>
+        )}
 
         <div>
+          <input {...register("email")} placeholder="Email" className="input" />
+          <p className="form-errors">{errors.email?.message}</p>
+        </div>
+
+        <div className="password-wrap">
           <input
             {...register("password")}
             placeholder="Password"
             type={showPassword ? "text" : "password"}
+            className="input"
           />
           {showPassword ? (
             <div onClick={() => setShowPassword(false)}>
-              <Icon name="openeye" className="" />
+              <Icon name="openeye" className="icon-eye" />
             </div>
           ) : (
             <div onClick={() => setShowPassword(true)}>
-              <Icon name="closeeye" className="" />
+              <Icon name="closeeye" className="icon-eye" />
             </div>
           )}
+          <p className="form-errors">{errors.password?.message}</p>
         </div>
-        <p>{errors.password?.message}</p>
+
+        {isInput && (
+          <div className="password-wrap">
+            <input
+              {...register("confirmPassword")}
+              placeholder="Confirm password"
+              type={showConfirmPassword ? "text" : "password"}
+              className="input"
+            />
+            {showConfirmPassword ? (
+              <div onClick={() => setShowConfirmPassword(false)}>
+                <Icon name="openeye" className="icon-eye" />
+              </div>
+            ) : (
+              <div onClick={() => setShowConfirmPassword(true)}>
+                <Icon name="closeeye" className="icon-eye" />
+              </div>
+            )}
+            <p className="form-errors">{errors.confirmPassword?.message}</p>
+          </div>
+        )}
       </div>
 
-      <button type="submit">Log in</button>
+      <button type="submit" className="form-button">
+        {buttonText}
+      </button>
+
+      <p className="form-text">
+        {formText}{" "}
+        <Link to={link} className="form-link">
+          {textLink}
+        </Link>
+      </p>
     </form>
   );
 };
