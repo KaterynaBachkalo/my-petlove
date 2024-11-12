9b434a0d33b22ade4acfac4e0cdb28be470308f4
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-12 20:42:39 +0200

    add

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,5 @@
-import { Link } from "react-router-dom";
-import Icon from "../components/Icon";
-
 const HomePage = () => {
-  return (
-    <Link to="/register">
-      <div className="background-image">
-        <p className="home-logo">
-          petl
-          <Icon
-            className="home-icon"
-            name="icon-heart-circle"
-            width={44}
-            height={44}
-          />
-          ve
-        </p>
-      </div>
-    </Link>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -5,16 +5,45 @@ const RegisterPage = () => {
   return (
     <>
       <div>
-        <img
-          srcSet="../img/authorization/registration/image@1x.png, ../img/authorization/registration/image@2x.png"
-          src="../img/authorization/registration/image@1x.png"
-          alt="cat"
-          className="registration-img"
-          width="335"
-          height="280"
-        />
-        <div>
-          <FormLogin />
+        <picture>
+          <source
+            media="(min-width: 1280px)"
+            srcSet="src/img/authorization/registration/desk/image@1x.png 1x, src/img/authorization/registration/desk/image@2x.png 2x"
+            type="image/png"
+            width={592}
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet="src/img/authorization/registration/tab/image@1x.png 1x, src/img/authorization/registration/tab/image@2x.png 2x"
+            type="image/png"
+            width={704}
+          />
+          <source
+            media="(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)"
+            srcSet="src/img/authorization/registration/image@1x.png 1x, src/img/authorization/registration/image@2x.png 2x"
+            type="image/png"
+          />
+          <img
+            src="src/img/authorization/registration/image@1x.png"
+            alt="cat"
+            className="registration-img"
+            width="335"
+            height="280"
+          />
+        </picture>
+
+        <div className="registration-wrap">
+          <h2 className="registration-title">Registration</h2>
+          <h3 className="registration-subtitle">
+            Thank you for your interest in our platform.
+          </h3>
+          <FormLogin
+            formText="Already have an account?"
+            link="/login"
+            textLink="Login"
+            buttonText="Registration"
+            isInput={true}
+          />
         </div>
       </div>
       <ToastContainer
