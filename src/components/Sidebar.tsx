0d42ea9a8475f034e0b7029d8e25a71d81eb2982
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-12 20:41:54 +0200

    add

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,51 +1,72 @@
 import { FC } from "react";
 import Icon from "./Icon";
 import { NavLink } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { AppDispatch } from "../../redux/store";
-// import { logOutThunk } from "../../redux/auth/operations";
+import { useSelector } from "react-redux";
+import { selectIsAuthenticated } from "../redux/auth/selectors";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../redux/store";
+import { logOutThunk } from "../redux/auth/operations";
 
 interface IProps {
   onClose: () => void;
   isOpen: boolean;
 }
 
-const Sidebar: FC<IProps> = ({ onClose }) => {
-  //   const dispatch = useDispatch<AppDispatch>();
+const Sidebar: FC<IProps> = ({ onClose, isOpen }) => {
+  const isAuthorized = useSelector(selectIsAuthenticated);
+  const dispatch = useDispatch<AppDispatch>();
 
   const handleLogOut = () => {
-    // dispatch(logOutThunk());
+    dispatch(logOutThunk());
   };
 
   return (
-    <div className="">
-      <nav className="">
-        <div onClick={onClose} className="">
-          <Icon name="close" width={32} height={32} className="" />
-        </div>
+    <div className={`menu ${isOpen ? "open" : ""}`}>
+      <div onClick={onClose} className="menu-close-wrap">
+        <Icon name="close" width={32} height={32} className="menu-close" />
+      </div>
 
-        <ul className="">
-          <li className="">
-            <NavLink to="/news" className="">
+      <nav className="menu-nav">
+        <ul className="menu-list">
+          <li>
+            <NavLink to="/news" className="menu-link">
               <p className="">News</p>
             </NavLink>
           </li>
-          <li className="">
-            <NavLink to="/notices" className="">
+          <li>
+            <NavLink to="/notices" className="menu-link">
               <p className="">Find pet</p>
             </NavLink>
           </li>
-          <li className="">
-            <NavLink to="/friends" className="">
+          <li>
+            <NavLink to="/friends" className="menu-link">
               <p className="">Our friends</p>
             </NavLink>
           </li>
         </ul>
       </nav>
 
-      <div className="" onClick={handleLogOut}>
-        <Icon name="" width={13} height={14} className="" />
-      </div>
+      {isAuthorized ? (
+        <div className="menu-auth-list">
+          <div onClick={handleLogOut} className="menu-link register">
+            <p className="menu-logout">Logout</p>
+          </div>
+        </div>
+      ) : (
+        <ul className="menu-auth-list">
+          <li className="menu-link login">
+            <NavLink to="/login" className="">
+              <p className="menu-login">Log in</p>
+            </NavLink>
+          </li>
+
+          <li className="menu-link register">
+            <NavLink to="/register" className="">
+              <p className="menu-register">Registration</p>
+            </NavLink>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
