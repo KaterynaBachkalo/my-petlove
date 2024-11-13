c393719ae9fea6c2888734eb802ccf942aa35569
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-13 12:12:22 +0200

    fix paths

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,11 @@
 import FormLogin from "../components/FormLogin";
 import { ToastContainer } from "react-toastify";
+import DeskImage1x from "../img/authorization/login/desk/image@1x.png";
+import DeskImage2x from "../img/authorization/login/desk/image@2x.png";
+import TabImage1x from "../img/authorization/login/tab/image@1x.png";
+import TabImage2x from "../img/authorization/login/tab/image@2x.png";
+import MobImage1x from "../img/authorization/login/image@1x.png";
+import MobImage2x from "../img/authorization/login/image@2x.png";
 
 const LoginPage = () => {
   return (
@@ -8,23 +14,23 @@ const LoginPage = () => {
         <picture>
           <source
             media="(min-width: 1280px)"
-            srcSet="src/img/authorization/login/desk/image@1x.png 1x, src/img/authorization/registration/desk/image@2x.png 2x"
+            srcSet={`${DeskImage1x} 1x, ${DeskImage2x} 2x`}
             type="image/png"
             width={592}
           />
           <source
             media="(min-width: 768px)"
-            srcSet="src/img/authorization/login/tab/image@1x.png 1x, src/img/authorization/registration/tab/image@2x.png 2x"
+            srcSet={`${TabImage1x} 1x, ${TabImage2x} 2x`}
             type="image/png"
             width={704}
           />
           <source
             media="(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)"
-            srcSet="src/img/authorization/login/image@1x.png 1x, src/img/authorization/registration/image@2x.png 2x"
+            srcSet={`${MobImage1x} 1x, ${MobImage2x} 2x`}
             type="image/png"
           />
           <img
-            src="src/img/authorization/login/image@1x.png"
+            src={MobImage1x}
             alt="dog"
             className="registration-img"
             width="335"
