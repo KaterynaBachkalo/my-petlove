335c308f5260e533986f73a96438b4c47c27fb14
KaterynaBachkalo <126788020+KaterynaBachkalo@users.noreply.github.com>
2024-11-13 10:07:22 +0000

    Deploying to gh-pages from @ KaterynaBachkalo/my-petlove@2e352d6512cdeaeb5ac9c5bc448abaa9281104a6 🚀

--- assets/NewsPage-Cbb1Lltw.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BklwyLy7.js";const r=()=>e.jsx("div",{children:"NewsPage"});export{r as default};
---
+++ assets/ProfilePage-CLx7mUfW.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B-bFYn5i.js";const o=()=>e.jsx("div",{children:"ProfilePage"});export{o as default};
