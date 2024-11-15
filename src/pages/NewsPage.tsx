63ffc1ab00e0b7ab64f25f314cf14e1e460bff7e
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-15 13:15:26 +0200

    fix

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,5 +1,51 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useSearchParams } from "react-router-dom";
+import { selectCurrentPage, selectNews } from "../redux/pet/selectors";
+import { useEffect } from "react";
+import { AppDispatch } from "../redux/store";
+import { fetchNews } from "../redux/pet/operations";
+import { FetchParams } from "../types";
+
 const NewsPage = () => {
-  return <div>NewsPage</div>;
+  const news = useSelector(selectNews);
+  console.log(news);
+
+  const limit = 6;
+
+  const currentPage = useSelector(selectCurrentPage);
+
+  const [searchParams] = useSearchParams();
+
+  const title = searchParams.get("title");
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const queryParams: FetchParams = {
+      page: currentPage,
+      limit: limit,
+      title: title ? title : null,
+    };
+    dispatch(fetchNews(queryParams));
+  }, [currentPage, dispatch, title]);
+
+  return (
+    <>
+      {news && news.length > 0 ? (
+        news.map(({ imageURL, title, text, date, url }) => (
+          <div key={url}>
+            <img src={imageURL} alt={title} />
+            <h3>{title}</h3>
+            <p>{text}</p>
+            <p>{date}</p>
+            <Link to={url}>Read more</Link>
+          </div>
+        ))
+      ) : (
+        <p>There are news yet</p>
+      )}
+    </>
+  );
 };
 
 export default NewsPage;
