826436c9beff80ea5a72d5bfbfce7c20fcda3c35
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-15 13:15:38 +0200

    fix

--- src/redux/pet/selectors.ts
+++ src/redux/pet/selectors.ts
@@ -1,19 +1,11 @@
 import { IState } from "./petSlice";
 import { IFilter } from "../filterSlice";
 
-export const selectProducts = (state: { admin: IState }) =>
-  state.admin.products;
+export const selectNews = (state: { admin: IState }) => state.admin.news;
 
-export const selectSuppliers = (state: { admin: IState }) =>
-  state.admin.suppliers;
+export const selectFriends = (state: { admin: IState }) => state.admin.friends;
 
-export const selectCustomers = (state: { admin: IState }) =>
-  state.admin.customers;
-
-export const selectIncomeExpenses = (state: { admin: IState }) =>
-  state.admin.incomeExpenses;
-
-export const selectOrders = (state: { admin: IState }) => state.admin.orders;
+export const selectNotices = (state: { admin: IState }) => state.admin.notices;
 
 export const selectIsLoading = (state: { admin: IState }) =>
   state.admin.isLoading;
@@ -25,14 +17,8 @@ export const selectFilter = (state: IFilter) => state.filter;
 export const selectCurrentPage = (state: { admin: IState }) =>
   state.admin.currentPage;
 
-export const selectTotalCustomers = (state: { admin: IState }) =>
-  state.admin.totalCustomers;
+export const selectTotalNews = (state: { admin: IState }) =>
+  state.admin.totalNews;
 
-export const selectTotalProducts = (state: { admin: IState }) =>
-  state.admin.totalProducts;
-
-export const selectTotalSuppliers = (state: { admin: IState }) =>
-  state.admin.totalSuppliers;
-
-export const selectTotalOrders = (state: { admin: IState }) =>
-  state.admin.totalOrders;
+export const selectTotalNotices = (state: { admin: IState }) =>
+  state.admin.totalNotices;
