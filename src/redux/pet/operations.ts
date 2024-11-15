dd1a07518641cf60512922c51a0c82644594f37f
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-15 13:15:09 +0200

    fix

--- src/redux/pet/operations.ts
+++ src/redux/pet/operations.ts
@@ -1,188 +1,138 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { petInstance } from "../auth/operations";
-import {
-  IProducts,
-  IProductsToBD,
-  ISuppliers,
-  ISuppliersToBD,
-} from "../../types";
+import { FetchParams } from "../../types";
 import { AxiosError } from "axios";
 
-interface FetchOrdersParams {
-  page: number;
-  limit: number;
-  name?: string;
-}
+export const fetchNews = createAsyncThunk(
+  "/news/fetchNews",
+  async ({ page, limit, title }: FetchParams, thunkAPI) => {
+    try {
+      const response = await petInstance.get("/news", {
+        params: { page, limit, title },
+      });
 
-export const fetchData = createAsyncThunk(
-  "/admin/dashboard/fetchData",
+      return response.data;
+    } catch (error: unknown) {
+      if (error instanceof AxiosError && error.message) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+      return thunkAPI.rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+export const fetchCities = createAsyncThunk(
+  "/cities/fetchCities",
   async (_, thunkAPI) => {
     try {
-      const response = await petInstance.get("/admin/dashboard");
-
+      const response = await petInstance.get("/cities");
       return response.data;
     } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
-export const fetchOrders = createAsyncThunk(
-  "/admin/orders/fetchOrders",
-  async ({ page, limit, name }: FetchOrdersParams, thunkAPI) => {
-    try {
-      const response = await petInstance.get("/admin/orders", {
-        params: { page, limit, name },
-      });
-      return response.data;
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
+      if (error instanceof AxiosError && error.message) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+      return thunkAPI.rejectWithValue("An unknown error occurred");
     }
   }
 );
 
-export const fetchProducts = createAsyncThunk(
-  "/admin/products/fetchProducts",
-  async ({ page, limit, name }: FetchOrdersParams, thunkAPI) => {
+export const fetchNotices = createAsyncThunk(
+  "/notices/fetchProducts",
+  async ({ page, limit, title }: FetchParams, thunkAPI) => {
     try {
-      const response = await petInstance.get("/admin/products", {
-        params: { page, limit, name },
+      const response = await petInstance.get("/notices", {
+        params: { page, limit, title },
       });
 
       return response.data;
     } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
+      if (error instanceof AxiosError && error.message) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+      return thunkAPI.rejectWithValue("An unknown error occurred");
     }
   }
 );
 
-export const fetchCustomers = createAsyncThunk(
-  "/admin/orders/fetchCustomers",
-  async ({ page, limit, name }: FetchOrdersParams, thunkAPI) => {
-    try {
-      const response = await petInstance.get("/admin/customers", {
-        params: { page, limit, name },
-      });
+// export const addProduct = createAsyncThunk(
+//   "products/addProduct",
+//   async (newProduct: IProductsToBD, thunkAPI) => {
+//     try {
+//       const response = await petInstance.post("/admin/products", newProduct);
 
-      return response.data;
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
+//       return response.data;
+//     } catch (error: unknown) {
+//       if (error instanceof AxiosError && error.message) {
+//         return thunkAPI.rejectWithValue(error.message);
+//       }
+//       return thunkAPI.rejectWithValue("An unknown error occurred");
+//     }
+//   }
+// );
 
-export const fetchSuppliers = createAsyncThunk(
-  "/admin/suppliers/fetchSuppliers",
-  async ({ page, limit, name }: FetchOrdersParams, thunkAPI) => {
-    try {
-      const response = await petInstance.get("/admin/suppliers", {
-        params: { page, limit, name },
-      });
+// export const deleteProduct = createAsyncThunk(
+//   "products/deleteProduct",
+//   async (productId: string, thunkAPI) => {
+//     try {
+//       await petInstance.delete(`/admin/products/${productId}`);
+//       return { productId };
+//     } catch (error: unknown) {
+//       if (error instanceof AxiosError && error.message) {
+//         return thunkAPI.rejectWithValue(error.message);
+//       }
+//       return thunkAPI.rejectWithValue("An unknown error occurred");
+//     }
+//   }
+// );
 
-      return response.data;
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
+// export const editProduct = createAsyncThunk(
+//   "products/editProduct",
+//   async (updatedProduct: IProducts, thunkAPI) => {
+//     try {
+//       const response = await petInstance.put(
+//         `/admin/products/${updatedProduct._id}`,
+//         updatedProduct
+//       );
+//       return response.data;
+//     } catch (error: unknown) {
+//       if (error instanceof AxiosError && error.message) {
+//         return thunkAPI.rejectWithValue(error.message);
+//       }
+//       return thunkAPI.rejectWithValue("An unknown error occurred");
+//     }
+//   }
+// );
 
-export const addProduct = createAsyncThunk(
-  "products/addProduct",
-  async (newProduct: IProductsToBD, thunkAPI) => {
-    try {
-      const response = await petInstance.post("/admin/products", newProduct);
+// export const addSupplier = createAsyncThunk(
+//   "suppliers/addSupplier",
+//   async (newSupplier: ISuppliersToBD, thunkAPI) => {
+//     try {
+//       const response = await petInstance.post("/admin/suppliers", newSupplier);
 
-      return response.data;
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
+//       return response.data;
+//     } catch (error: unknown) {
+//       if (error instanceof AxiosError && error.message) {
+//         return thunkAPI.rejectWithValue(error.message);
+//       }
+//       return thunkAPI.rejectWithValue("An unknown error occurred");
+//     }
+//   }
+// );
 
-export const deleteProduct = createAsyncThunk(
-  "products/deleteProduct",
-  async (productId: string, thunkAPI) => {
-    try {
-      await petInstance.delete(`/admin/products/${productId}`);
-      return { productId };
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
-
-export const editProduct = createAsyncThunk(
-  "products/editProduct",
-  async (updatedProduct: IProducts, thunkAPI) => {
-    try {
-      const response = await petInstance.put(
-        `/admin/products/${updatedProduct._id}`,
-        updatedProduct
-      );
-      return response.data;
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
-
-export const addSupplier = createAsyncThunk(
-  "suppliers/addSupplier",
-  async (newSupplier: ISuppliersToBD, thunkAPI) => {
-    try {
-      const response = await petInstance.post(
-        "/admin/suppliers",
-        newSupplier
-      );
-
-      return response.data;
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
-
-export const editSupplier = createAsyncThunk(
-  "suppliers/editSupplier",
-  async (updatedSupplier: ISuppliers, thunkAPI) => {
-    try {
-      const response = await petInstance.put(
-        `/admin/suppliers/${updatedSupplier._id}`,
-        updatedSupplier
-      );
-      console.log(response.data);
-      return response.data;
-    } catch (error: unknown) {
-        if (error instanceof AxiosError && error.message) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
-        return thunkAPI.rejectWithValue("An unknown error occurred");
-    }
-  }
-);
+// export const editSupplier = createAsyncThunk(
+//   "suppliers/editSupplier",
+//   async (updatedSupplier: ISuppliers, thunkAPI) => {
+//     try {
+//       const response = await petInstance.put(
+//         `/admin/suppliers/${updatedSupplier._id}`,
+//         updatedSupplier
+//       );
+//       console.log(response.data);
+//       return response.data;
+//     } catch (error: unknown) {
+//       if (error instanceof AxiosError && error.message) {
+//         return thunkAPI.rejectWithValue(error.message);
+//       }
+//       return thunkAPI.rejectWithValue("An unknown error occurred");
+//     }
+//   }
+// );
