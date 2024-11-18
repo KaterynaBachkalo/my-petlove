906ba5f81c73af3df5d17092a8fdad8208e9fdae
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-18 17:59:07 +0200

    add

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,50 +1,61 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useSearchParams } from "react-router-dom";
-import { selectCurrentPage, selectNews } from "../redux/pet/selectors";
-import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { selectNews, selectCurrentPage } from "../redux/pet/selectors";
+import { useEffect, useState } from "react";
 import { AppDispatch } from "../redux/store";
 import { fetchNews } from "../redux/pet/operations";
 import { FetchParams } from "../types";
+import FindForm from "../components/FindForm";
+import { formatData } from "../utils/formatData";
 
 const NewsPage = () => {
   const news = useSelector(selectNews);
-  console.log(news);
 
   const limit = 6;
 
   const currentPage = useSelector(selectCurrentPage);
 
-  const [searchParams] = useSearchParams();
-
-  const title = searchParams.get("title");
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     const queryParams: FetchParams = {
       page: currentPage,
-      limit: limit,
-      title: title ? title : null,
+      limit,
+      title: searchQuery ? searchQuery : null,
     };
     dispatch(fetchNews(queryParams));
-  }, [currentPage, dispatch, title]);
+  }, [currentPage, dispatch, searchQuery]);
 
   return (
-    <>
-      {news && news.length > 0 ? (
-        news.map(({ imageURL, title, text, date, url }) => (
-          <div key={url}>
-            <img src={imageURL} alt={title} />
-            <h3>{title}</h3>
-            <p>{text}</p>
-            <p>{date}</p>
-            <Link to={url}>Read more</Link>
-          </div>
-        ))
-      ) : (
-        <p>There are news yet</p>
-      )}
-    </>
+    <div className="news-container">
+      <div className="news-main-wrap">
+        <h2 className="news-main-title">News</h2>
+        <FindForm setSearchQuery={setSearchQuery} />
+      </div>
+      <div className="news-cards">
+        {news && news.length > 0 ? (
+          news.map(({ imgUrl, title, text, date, url }) => (
+            <div key={url} className="news-card">
+              <div className="news-card-top">
+                <img src={imgUrl} alt={title} className="news-img" />
+                <h3 className="news-title">{title}</h3>
+                <p className="news-text">{text}</p>
+              </div>
+              <div className="news-wrap">
+                <p className="news-date">{formatData(date)}</p>
+                <Link to={url} className="news-link">
+                  Read more
+                </Link>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>There are news yet</p>
+        )}
+      </div>
+    </div>
   );
 };
 
