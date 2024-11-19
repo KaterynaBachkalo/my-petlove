e6ef000db31632b5c5ca925218af79d3708224dc
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-19 23:44:26 +0200

    fix

--- src/pages/NoticesPage.tsx
+++ src/pages/NoticesPage.tsx
@@ -8,6 +8,7 @@ import FindForm from "../components/FindForm";
 import Icon from "../components/Icon";
 import FindFormList from "../components/FindFormList";
 import SortButton from "../components/SortButton";
+import { fixDate } from "../utils/formatDate";
 
 const NoticesPage = () => {
   const notices = useSelector(selectNotices);
@@ -32,20 +33,24 @@ const NoticesPage = () => {
   return (
     <div className="notices-container">
       <div className="notices-main-wrap">
-        <h2 className="news-main-title">Find your favorite pet</h2>
+        <h2 className="notices-main-title">Find your favorite pet</h2>
         <div className="notices-form-wrap">
           <div className="notices-form-top">
             <FindForm setSearchQuery={setSearchQuery} placeholder="Search" />
-            <div className="wrap-form-lists">
-              <FindFormList
-                setSearchQuery={setSearchQuery}
-                placeholder="Category"
-              />
-              <FindFormList
-                setSearchQuery={setSearchQuery}
-                placeholder="By gender"
-              />
-            </div>
+
+            <FindFormList
+              setSearchQuery={setSearchQuery}
+              placeholder="Category"
+            />
+            <FindFormList
+              setSearchQuery={setSearchQuery}
+              placeholder="By gender"
+            />
+            <FindFormList
+              setSearchQuery={setSearchQuery}
+              placeholder="By type"
+            />
+
             <FindForm setSearchQuery={setSearchQuery} placeholder="Location" />
           </div>
           <div className="notices-stroke"></div>
@@ -57,7 +62,7 @@ const NoticesPage = () => {
           </div>
         </div>
       </div>
-      <div className="news-cards">
+      <div className="news-cards notices-cards">
         {notices && notices.length > 0 ? (
           notices.map(
             ({
@@ -78,34 +83,41 @@ const NoticesPage = () => {
                   <h3 className="notices-title">{title}</h3>
                   <div className="notices-icon-wrap">
                     <Icon className="icon-star" name="star" />
-                    <p>{popularity}</p>
+                    <p className="notices-popularity">{popularity}</p>
                   </div>
                 </div>
                 <div className="notices-text-wrapper">
                   <div className="notices-text-wrap">
-                    <p>Name</p>
+                    <p className="notices-title-text">Name</p>
                     <p>{name}</p>
                   </div>
                   <div className="notices-text-wrap">
-                    <p>Birthday</p>
-                    <p>{birthday}</p>
+                    <p className="notices-title-text">Birthday</p>
+                    <p>{fixDate(birthday)}</p>
                   </div>
                   <div className="notices-text-wrap">
-                    <p>Sex</p>
+                    <p className="notices-title-text">Sex</p>
                     <p>{sex}</p>
                   </div>
                   <div className="notices-text-wrap">
-                    <p>Species</p>
+                    <p className="notices-title-text">Species</p>
                     <p>{species}</p>
                   </div>
                   <div className="notices-text-wrap">
-                    <p>Category</p>
+                    <p className="notices-title-text">Category</p>
                     <p>{category}</p>
                   </div>
                 </div>
                 <p className="notices-text">{comment}</p>
 
-                <button type="button">Read more</button>
+                <div className="notices-button-wrap">
+                  <button type="button" className="notices-button">
+                    Read more
+                  </button>
+                  <div className="notices-add-favorite">
+                    <Icon className="icon-heart" name="heart" />
+                  </div>
+                </div>
               </div>
             )
           )
