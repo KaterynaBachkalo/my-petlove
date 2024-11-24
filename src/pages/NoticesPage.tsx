dd7673c2d6332355ca7ff6ec628722c9f8360b09
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-24 13:51:44 +0200

    fix

--- src/pages/NoticesPage.tsx
+++ src/pages/NoticesPage.tsx
@@ -1,14 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectCurrentPage, selectNotices } from "../redux/pet/selectors";
+import {
+  selectCurrentPage,
+  selectNotices,
+  selectTotalNotices,
+} from "../redux/pet/selectors";
 import { useEffect, useState } from "react";
 import { AppDispatch } from "../redux/store";
 import { fetchNotices } from "../redux/pet/operations";
 import { FetchParams } from "../types";
 import FindForm from "../components/FindForm";
-import Icon from "../components/Icon";
 import FindFormList from "../components/FindFormList";
 import SortButton from "../components/SortButton";
-import { fixDate } from "../utils/formatDate";
+import Pagination from "../components/Pagination";
+import NoticeCard from "../components/NoticeCard";
 
 const NoticesPage = () => {
   const notices = useSelector(selectNotices);
@@ -16,6 +20,7 @@ const NoticesPage = () => {
   const limit = 6;
 
   const currentPage = useSelector(selectCurrentPage);
+  const totalNotices = useSelector(selectTotalNotices);
 
   const [searchQuery, setSearchQuery] = useState<string>("");
 
@@ -76,55 +81,26 @@ const NoticesPage = () => {
               comment,
               popularity,
             }) => (
-              <div key={title} className="notices-card">
-                <img src={imgURL} alt={title} className="notices-img" />
-
-                <div className="notices-title-wrap">
-                  <h3 className="notices-title">{title}</h3>
-                  <div className="notices-icon-wrap">
-                    <Icon className="icon-star" name="star" />
-                    <p className="notices-popularity">{popularity}</p>
-                  </div>
-                </div>
-                <div className="notices-text-wrapper">
-                  <div className="notices-text-wrap">
-                    <p className="notices-title-text">Name</p>
-                    <p>{name}</p>
-                  </div>
-                  <div className="notices-text-wrap">
-                    <p className="notices-title-text">Birthday</p>
-                    <p>{fixDate(birthday)}</p>
-                  </div>
-                  <div className="notices-text-wrap">
-                    <p className="notices-title-text">Sex</p>
-                    <p>{sex}</p>
-                  </div>
-                  <div className="notices-text-wrap">
-                    <p className="notices-title-text">Species</p>
-                    <p>{species}</p>
-                  </div>
-                  <div className="notices-text-wrap">
-                    <p className="notices-title-text">Category</p>
-                    <p>{category}</p>
-                  </div>
-                </div>
-                <p className="notices-text">{comment}</p>
-
-                <div className="notices-button-wrap">
-                  <button type="button" className="notices-button">
-                    Read more
-                  </button>
-                  <div className="notices-add-favorite">
-                    <Icon className="icon-heart" name="heart" />
-                  </div>
-                </div>
-              </div>
+              <NoticeCard
+                key={title}
+                name={name}
+                title={title}
+                imgURL={imgURL}
+                species={species}
+                birthday={birthday}
+                sex={sex}
+                category={category}
+                comment={comment}
+                popularity={popularity}
+              />
             )
           )
         ) : (
           <p>There are news yet</p>
         )}
       </div>
+
+      <Pagination totalItems={totalNotices} />
     </div>
   );
 };
