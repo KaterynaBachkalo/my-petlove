470f6b67a96e3dcf9955db73b8a72089381c1a2a
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-24 13:51:26 +0200

    fix universal component Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,32 +1,36 @@
 import { FC } from "react";
 import ReactPaginate from "react-paginate";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../redux/store";
 import { setCurrentPage } from "../redux/pet/petSlice";
+import { selectCurrentPage } from "../redux/pet/selectors";
 
 interface IProps {
-  currentPage: number;
-  totalPages: number;
-  onPageChange: (page: number) => void;
   totalItems: number;
 }
 
-const Pagination: FC<IProps> = ({
-  totalPages,
-  onPageChange,
-  currentPage,
-  totalItems,
-}) => {
-  const handlePageClick = (event: { selected: number }) => {
-    onPageChange(event.selected); // Pass the selected page to the parent
+const Pagination: FC<IProps> = ({ totalItems }) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const currentPage = useSelector(selectCurrentPage);
+
+  const limit = 6;
+
+  const totalPages = Math.max(1, Math.ceil(totalItems / limit));
+
+  const handlePageChange = (newPage: number) => {
+    dispatch(setCurrentPage(newPage));
   };
-  const getLastPageNumber = (n: number) => {
-    return n % 10 === 0 ? n / 10 - 1 : Math.floor(n / 10);
+
+  const handlePageClick = (event: { selected: number }) => {
+    handlePageChange(event.selected + 1);
   };
 
   return (
     <div className="pagination-container">
       <button
-        disabled={currentPage === 0}
-        onClick={() => setCurrentPage(0)}
+        disabled={currentPage === 1}
+        onClick={() => handlePageChange(1)}
         className="first-page"
       >
         {`<<`}{" "}
@@ -34,7 +38,7 @@ const Pagination: FC<IProps> = ({
       <ReactPaginate
         previousLabel={"<"}
         nextLabel={">"}
-        breakLabel={<a href="">...</a>}
+        breakLabel={<span>...</span>}
         breakClassName={"break-me"}
         pageCount={totalPages}
         marginPagesDisplayed={2}
@@ -42,15 +46,15 @@ const Pagination: FC<IProps> = ({
         onPageChange={handlePageClick}
         containerClassName={"pagination"}
         activeClassName={"active"}
-        forcePage={currentPage}
+        forcePage={currentPage - 1}
         nextClassName={`next ${
           currentPage >= totalPages - 1 ? "disabled" : ""
         }`}
       />
       <button
-        disabled={currentPage === getLastPageNumber(totalItems)}
+        disabled={currentPage === totalPages}
         className="last-page"
-        onClick={() => setCurrentPage(getLastPageNumber(totalItems))}
+        onClick={() => handlePageChange(totalPages)}
       >
         {`>>`}{" "}
       </button>
