12fd5ff0da337b1efb34e1acb9179ea856805cb5
KaterynaBachkalo <kozlovets88@gmail.com>
2024-11-25 22:18:18 +0200

    add

---
+++ src/data/types.ts
@@ -0,0 +1,16 @@
+export const types = [
+  "dog",
+  "cat",
+  "monkey",
+  "bird",
+  "snake",
+  "turtle",
+  "lizard",
+  "frog",
+  "fish",
+  "ants",
+  "bees",
+  "butterfly",
+  "spider",
+  "scorpion",
+];
