8a1e24f623fad414d438459547d509295a7c0a1f
KaterynaBachkalo <kozlovets88@gmail.com>
2024-12-01 14:02:51 +0200

    add progress

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,22 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Icon from "../components/Icon";
+import ProgressBar from "../components/ProgressBar";
+import { useState } from "react";
 
 const FirstPage = () => {
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    setLoading(true);
+  };
+
+  const handleProgressComplete = () => {
+    setLoading(false);
+    navigate("/home"); // Перехід на сторінку home.
+  };
+
   return (
-    <Link to="/home">
-      <div className="background-image">
-        <p className="first-logo">
-          petl
-          <Icon
-            className="first-icon"
-            name="icon-heart-circle"
-            width={44}
-            height={44}
-          />
-          ve
-        </p>
-      </div>
-    </Link>
+    <>
+      {loading && <ProgressBar onComplete={handleProgressComplete} />}
+
+      <Link to="/home" className="first-link" onClick={handleClick}>
+        <div className="background-image">
+          {!loading && (
+            <p className="first-logo">
+              petl
+              <Icon
+                className="first-icon"
+                name="icon-heart-circle"
+                width={44}
+                height={44}
+              />
+              ve
+            </p>
+          )}
+        </div>
+      </Link>
+    </>
   );
 };
 
