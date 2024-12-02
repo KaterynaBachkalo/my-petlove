7aa99c4b6744fa9c43601eafa267bd112d08cd15
KaterynaBachkalo <kozlovets88@gmail.com>
2024-12-02 20:15:10 +0200

    add pade to url

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,36 +1,26 @@
 import { FC } from "react";
 import ReactPaginate from "react-paginate";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "../redux/store";
-import { setCurrentPage } from "../redux/pet/petSlice";
-import { selectCurrentPage } from "../redux/pet/selectors";
 
 interface IProps {
   totalItems: number;
+  currentPage: number;
+  onPageChange: (value: number) => void;
 }
 
-const Pagination: FC<IProps> = ({ totalItems }) => {
-  const dispatch = useDispatch<AppDispatch>();
-
-  const currentPage = useSelector(selectCurrentPage);
-
+const Pagination: FC<IProps> = ({ totalItems, currentPage, onPageChange }) => {
   const limit = 6;
 
   const totalPages = Math.max(1, Math.ceil(totalItems / limit));
 
-  const handlePageChange = (newPage: number) => {
-    dispatch(setCurrentPage(newPage));
-  };
-
   const handlePageClick = (event: { selected: number }) => {
-    handlePageChange(event.selected + 1);
+    onPageChange(event.selected + 1);
   };
 
   return (
     <div className="pagination-container">
       <button
         disabled={currentPage === 1}
-        onClick={() => handlePageChange(1)}
+        onClick={() => onPageChange(1)}
         className="first-page"
       >
         {`<<`}{" "}
@@ -54,7 +44,7 @@ const Pagination: FC<IProps> = ({ totalItems }) => {
       <button
         disabled={currentPage === totalPages}
         className="last-page"
-        onClick={() => handlePageChange(totalPages)}
+        onClick={() => onPageChange(totalPages)}
       >
         {`>>`}{" "}
       </button>
