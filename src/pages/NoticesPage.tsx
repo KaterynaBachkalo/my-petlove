e3965caf3a3cf97a31fb5a345c189b4a08ffdc9b
KaterynaBachkalo <kozlovets88@gmail.com>
2025-01-21 20:31:27 +0200

    fix type of sorting/ Sorting is on the backend

--- src/pages/NoticesPage.tsx
+++ src/pages/NoticesPage.tsx
@@ -1,49 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
-import {
-  selectFilter,
-  selectNotices,
-  selectTotalNotices,
-} from "../redux/pet/selectors";
+import { selectNotices, selectTotalNotices } from "../redux/pet/selectors";
 import { useEffect, useState } from "react";
 import { AppDispatch } from "../redux/store";
 import { fetchNotices } from "../redux/pet/operations";
-import { FetchParams, INotice, ISearchQuery } from "../types";
+import { FetchParams, ISearchQuery } from "../types";
 import Pagination from "../components/Pagination";
 import NoticeCard from "../components/NoticeCard";
 import FilterForms from "../components/FilterForms";
 import { useSearchParams } from "react-router-dom";
+import { clearState } from "../redux/pet/petSlice";
 
 const NoticesPage = () => {
   const notices = useSelector(selectNotices);
-
-  const filter = useSelector(selectFilter);
-
-  const [sortedNotices, setSortedNotices] = useState<INotice[]>(notices);
-
-  useEffect(() => {
-    if (notices.length > 0) {
-      let sortedNotices = [...notices];
-
-      switch (filter) {
-        case "Popular":
-          sortedNotices.sort((a, b) => b.popularity - a.popularity);
-          break;
-        case "Unpopular":
-          sortedNotices.sort((a, b) => a.popularity - b.popularity);
-          break;
-        case "Cheap":
-          sortedNotices.sort((a, b) => (a.price || 0) - (b.price || 0));
-          break;
-        case "Expensive":
-          sortedNotices.sort((a, b) => (b.price || 0) - (a.price || 0));
-          break;
-        default:
-          sortedNotices = notices;
-      }
-
-      setSortedNotices(sortedNotices);
-    }
-  }, [notices, filter]);
 
   const limit = 6;
 
@@ -64,6 +32,7 @@ const NoticesPage = () => {
     category: searchParams.get("category") || null,
     sex: searchParams.get("sex") || null,
     species: searchParams.get("species") || null,
+    sort: searchParams.get("sort") || null,
   });
 
   const dispatch = useDispatch<AppDispatch>();
@@ -76,7 +45,9 @@ const NoticesPage = () => {
       category: searchQuery?.category ? searchQuery.category : null,
       sex: searchQuery?.sex ? searchQuery.sex : null,
       species: searchQuery?.species ? searchQuery.species : null,
+      sort: searchQuery?.sort ? searchQuery?.sort : null,
     };
+    dispatch(clearState());
     dispatch(fetchNotices(queryParams));
   }, [currentPage, dispatch, searchQuery]);
 
@@ -97,8 +68,8 @@ const NoticesPage = () => {
         <FilterForms setSearchQuery={setSearchQuery} />
       </div>
       <div className="news-cards notices-cards">
-        {sortedNotices && sortedNotices.length > 0 ? (
-          sortedNotices.map(
+        {notices && notices.length > 0 ? (
+          notices.map(
             ({
               name,
               title,
