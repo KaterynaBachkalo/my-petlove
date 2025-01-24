367b55df5fe2c6b898a8239a367600b39c3e4377
KaterynaBachkalo <kozlovets88@gmail.com>
2025-01-24 13:08:31 +0200

    add madal component

---
+++ src/components/Modals/Modal.tsx
@@ -0,0 +1,45 @@
+import { FC, ReactNode, useRef } from "react";
+import { createPortal } from "react-dom";
+import useCloseModals from "../../utils/useCloseModals";
+import Icon from "../Icon";
+
+const modalRoot: Element | null = document.querySelector("#root-modal");
+
+interface IProps {
+  onClose: (value: boolean) => void;
+  children: ReactNode;
+}
+
+const Modal: FC<IProps> = ({ onClose, children }) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+  useCloseModals(onClose, modalRef);
+
+  return (
+    modalRoot &&
+    createPortal(
+      <div className="modal-backdrop">
+        <div className="modal-container">
+          <div
+            className="modal"
+            ref={modalRef}
+            onClick={(event) => event.stopPropagation()}
+          >
+            <button className="modal-btnClose" onClick={() => onClose(false)}>
+              <Icon
+                className="modal-icon"
+                name="close"
+                width={20}
+                height={20}
+              />
+            </button>
+            <div>{children}</div>
+          </div>
+        </div>
+      </div>,
+
+      modalRoot
+    )
+  );
+};
+
+export default Modal;
