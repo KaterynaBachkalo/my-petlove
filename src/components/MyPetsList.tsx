39226067bdc119a632d3b5cc0c1c68fabbf107c1
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-09 22:34:17 +0200

    fix

---
+++ src/components/MyPetsList.tsx
@@ -0,0 +1,9 @@
+const MyPetsList = () => {
+  return (
+    <section className="my-pets-list">
+      <p className="favorites-no-pets">No my pets yet :(</p>
+    </section>
+  );
+};
+
+export default MyPetsList;
