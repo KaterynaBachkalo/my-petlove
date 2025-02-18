ea64cb75b4ccf3903603acc0fdd696fb5d3ce0d8
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-18 13:51:44 +0200

    add context

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
-import { createRoot } from 'react-dom/client';
-import './scss/index.scss';
-import App from './App.tsx';
+import { createRoot } from "react-dom/client";
+import "./scss/index.scss";
+import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.ts";
 import { Provider } from "react-redux";
+import { createContext } from "react";
+import { IContextTheme } from "./types.ts";
+import ThemeProvider from "./contexts/ThemeProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+export const ThemeContext = createContext<IContextTheme | undefined>(undefined);
+
+createRoot(document.getElementById("root")!).render(
   <>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter basename="/my-petlove">
-          <App />
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </>,
-)
+  </>
+);
