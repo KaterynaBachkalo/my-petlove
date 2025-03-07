fd0be541c17d028a43413b6e1216b7e239b4c156
KaterynaBachkalo <kozlovets88@gmail.com>
2025-03-07 16:02:05 +0200

    fix

--- src/components/ProfileComponents/MyInformationForm.tsx
+++ src/components/ProfileComponents/MyInformationForm.tsx
@@ -5,6 +5,16 @@ const MyInformationForm = () => {
   const currentUser = useSelector(selectCurrentUser);
 
   const { name, email, phone } = currentUser;
+
+  const formatPhoneNumber = (phoneNumber: number | null) => {
+    const phoneStr = phoneNumber?.toString();
+    if (phoneStr) {
+      return phoneStr?.replace(/(\d{2})(\d{3})(\d{2})(\d{2})/, "$1 $2 $3 $4");
+    }
+  };
+
+  const phoneNumber = formatPhoneNumber(phone);
+
   return (
     <>
       <h2 className="profile-title">My information</h2>
@@ -12,7 +22,7 @@ const MyInformationForm = () => {
       <form className="wrap-input profile">
         <input
           type="text"
-          value={name ?? "NONAME"}
+          value={!name ? "User name" : name}
           className="input profile"
           readOnly
         />
@@ -24,7 +34,7 @@ const MyInformationForm = () => {
         />
         <input
           type="tel"
-          value={phone ?? "+380"}
+          value={`+380 ${phoneNumber}`}
           className="input profile"
           readOnly
         />
