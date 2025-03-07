3f1c29d803fe02a2f7a4473a840e93b39f5b2581
KaterynaBachkalo <kozlovets88@gmail.com>
2025-03-07 16:00:36 +0200

    add

--- src/components/GenderButtons.tsx
+++ src/components/GenderButtons.tsx
@@ -3,38 +3,48 @@ import Icon from "./ComponentsForDesign/Icon";
 
 interface IProps {
   handleCheckGender: (value: string) => void;
+  selectedGender: string | null;
 }
 
-const GenderButtons: FC<IProps> = ({ handleCheckGender }) => {
+const GenderButtons: FC<IProps> = ({ handleCheckGender, selectedGender }) => {
   return (
     <>
       <div className="addPet-icon-wrap">
         <div
-          className="addPet-icon woman"
+          className={`addPet-icon woman ${
+            selectedGender === "woman" ? "active" : ""
+          }`}
           onClick={() => handleCheckGender("woman")}
         >
           <Icon
             name="icon-female"
-            className="icon-woman"
-            width={20}
-            height={20}
+            className={`icon-woman ${
+              selectedGender === "woman" ? "active" : ""
+            }`}
           />
         </div>
         <div
-          className="addPet-icon man"
+          className={`addPet-icon man ${
+            selectedGender === "man" ? "active" : ""
+          }`}
           onClick={() => handleCheckGender("man")}
         >
-          <Icon name="icon-male" className="icon-man" width={20} height={20} />
+          <Icon
+            name="icon-male"
+            className={`icon-man ${selectedGender === "man" ? "active" : ""}`}
+          />
         </div>
         <div
-          className="addPet-icon man-woman"
+          className={`addPet-icon man-woman ${
+            selectedGender === "man-woman" ? "active" : ""
+          }`}
           onClick={() => handleCheckGender("man-woman")}
         >
           <Icon
             name="icon-male-female"
-            className="icon-man-woman"
-            width={20}
-            height={20}
+            className={`icon-man-woman ${
+              selectedGender === "man-woman" ? "active" : ""
+            }`}
           />
         </div>
       </div>
