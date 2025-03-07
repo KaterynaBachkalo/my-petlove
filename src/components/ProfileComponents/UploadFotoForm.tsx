e99bbb0649d1f4e2fd5063dfdf672f1ce71bddc9
KaterynaBachkalo <kozlovets88@gmail.com>
2025-03-07 16:02:24 +0200

    fix

--- src/components/ProfileComponents/UploadFotoForm.tsx
+++ src/components/ProfileComponents/UploadFotoForm.tsx
@@ -1,25 +1,66 @@
-import { ChangeEvent, forwardRef } from "react";
+import { ChangeEvent, forwardRef, useState } from "react";
 import Icon from "../ComponentsForDesign/Icon";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../redux/auth/selectors";
 import { useLocation } from "react-router-dom";
+import { decodeFileName } from "../../utils/decodeFileName";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 interface IProps {
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (event: ChangeEvent<HTMLInputElement> | File) => void;
   isModal: boolean;
-  petAvatarUrl?: string | null;
 }
 
 const UploadFotoForm = forwardRef<HTMLInputElement, IProps>(
-  ({ onChange, isModal, petAvatarUrl }, ref) => {
+  ({ onChange, isModal }, ref) => {
+    const [url, setUrl] = useState<string>("");
+
+    const location = useLocation();
+
     const currentUser = useSelector(selectCurrentUser);
     const userAvatarURL = currentUser.avatar;
+
+    const formattedUserAvatarURL = userAvatarURL
+      .split("_")
+      .slice(1, 3)
+      .join("_");
+
+    const correctName = decodeFileName(formattedUserAvatarURL);
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
       onChange(event);
     };
 
-    const location = useLocation();
+    const handleUrlChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setUrl(e.target.value);
+    };
+
+    const fetchImageFromUrl = async () => {
+      if (!url) return;
+
+      try {
+        const proxyUrl = `http://localhost:4000/api/proxy?url=${encodeURIComponent(
+          url
+        )}`;
+
+        const response = await axios.get(proxyUrl, { responseType: "blob" });
+
+        const blob = response.data;
+
+        const file = new File([blob], url, {
+          type: blob.type,
+        });
+
+        onChange(file);
+      } catch {
+        toast.error("Error loading image by URL");
+      }
+    };
+
+    const handleBlur = () => {
+      fetchImageFromUrl();
+    };
 
     return (
       <form>
@@ -35,17 +76,16 @@ const UploadFotoForm = forwardRef<HTMLInputElement, IProps>(
         </label>
 
         {isModal && (
-          <div className="selected-file-input-wrap">
+          <div className="selected-file-input-wrap my-pet">
             <input
               type="text"
               value={
-                location.pathname === "/add-pet"
-                  ? petAvatarUrl || ""
-                  : userAvatarURL || ""
+                location.pathname === "/add-pet" ? undefined : correctName || ""
               }
               className="selected-file-input"
-              placeholder="No file chosen"
-              readOnly
+              placeholder="Enter URL"
+              onChange={handleUrlChange}
+              onBlur={handleBlur}
             />
             <label htmlFor="inputFile" className="uploadPhoto">
               <div className="profile-upload-wrap">
