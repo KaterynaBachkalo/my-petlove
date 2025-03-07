dafb78ebd7e87980e0cda0e7aca6cb0feb768430
KaterynaBachkalo <kozlovets88@gmail.com>
2025-03-07 16:03:02 +0200

    fix

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -6,32 +6,22 @@ import TabImage1x from "../img/authorization/registration/tab/image@1x.png";
 import TabImage2x from "../img/authorization/registration/tab/image@2x.png";
 import MobImage1x from "../img/authorization/registration/image@1x.png";
 import MobImage2x from "../img/authorization/registration/image@2x.png";
+import Picture from "../components/Picture";
 
 const RegisterPage = () => {
   return (
     <>
       <div className="auth-wrapper">
-        <picture>
-          <source
-            media="(min-width: 1280px)"
-            srcSet={`${DeskImage1x} 1x, ${DeskImage2x} 2x`}
-            type="image/png"
-            width={592}
-            height={654}
-          />
-          <source
-            media="(min-width: 768px)"
-            srcSet={`${TabImage1x} 1x, ${TabImage2x} 2x`}
-            type="image/png"
-            width={704}
-          />
-          <source
-            media="(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)"
-            srcSet={`${MobImage1x} 1x, ${MobImage2x} 2x`}
-            type="image/png"
-          />
-          <img src={MobImage1x} alt="cat" className="registration-img" />
-        </picture>
+        <Picture
+          mob1x={MobImage1x}
+          mob2x={MobImage2x}
+          tab1x={TabImage1x}
+          tab2x={TabImage2x}
+          desk1x={DeskImage1x}
+          desk2x={DeskImage2x}
+          alt="Cat"
+          className="registration-img"
+        />
 
         <div className="registration-wrap">
           <h2 className="registration-title">Registration</h2>
