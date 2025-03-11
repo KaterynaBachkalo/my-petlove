7faa245e144dca95c83eecf801a6f017568fe2a9
KaterynaBachkalo <kozlovets88@gmail.com>
2025-03-11 12:02:46 +0200

    fix

--- src/components/SwitchThemeButtons.tsx
+++ src/components/SwitchThemeButtons.tsx
@@ -1,20 +1,27 @@
+import { useLocation } from "react-router-dom";
 import { useTheme } from "../utils/useTheme";
 import Icon from "./ComponentsForDesign/Icon";
 
 const SwitchThemeButtons = () => {
+  const location = useLocation();
+
   const { theme, tongleTheme } = useTheme();
   return (
     <div className="switch-buttons">
       <button type="button" onClick={tongleTheme} className="switch-button">
         <Icon
           name="icon-sun"
-          className={`switch-icon ${theme === "light" ? "dark" : "light"}`}
+          className={`switch-icon ${theme === "light" ? "white" : "dark"} ${
+            location.pathname === "/home" ? "home" : ""
+          }`}
         />
       </button>
       <button type="button" onClick={tongleTheme} className="switch-button">
         <Icon
           name="icon-moon"
-          className={`switch-icon ${theme === "light" ? "light" : "dark"}`}
+          className={`switch-icon ${theme === "light" ? "accent" : "light"} ${
+            location.pathname === "/home" ? "home" : ""
+          }`}
         />
       </button>
     </div>
@@ -22,3 +29,26 @@ const SwitchThemeButtons = () => {
 };
 
 export default SwitchThemeButtons;
+
+{
+  /* <Icon
+          name="icon-sun"
+          className={`switch-icon ${
+            location.pathname !== "/home"
+              ? ""
+              : theme === "light"
+              ? "home white"
+              : "home dark"
+          }`}
+        />
+        <Icon
+        name="icon-moon"
+        className={`switch-icon ${
+          location.pathname !== "/home"
+            ? ""
+            : theme === "light"
+            ? "home accent"
+            : "home light"
+        }`}
+      /> */
+}
