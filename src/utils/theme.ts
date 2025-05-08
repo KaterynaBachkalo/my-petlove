ac3a770dd9fd8239354273d1dafcb667036cdeaa
KaterynaBachkalo <kozlovets88@gmail.com>
2025-05-08 13:16:21 +0300

    add theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const mytheme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 320,
+      sm: 375,
+      md: 768,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
