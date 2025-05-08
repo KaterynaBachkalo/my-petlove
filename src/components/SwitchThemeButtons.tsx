b2094bdc5a1b445cbb071243c05a75a4fd2e7ac3
KaterynaBachkalo <kozlovets88@gmail.com>
2025-05-08 13:14:16 +0300

    add

--- src/components/SwitchThemeButtons.tsx
+++ src/components/SwitchThemeButtons.tsx
@@ -5,10 +5,23 @@ import Icon from "./ComponentsForDesign/Icon";
 const SwitchThemeButtons = () => {
   const location = useLocation();
 
-  const { theme, tongleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
+
+  const myToggleThemeLight = () => {
+    if (theme === "dark") toggleTheme();
+  };
+
+  const myToggleThemeDark = () => {
+    if (theme === "light") toggleTheme();
+  };
+
   return (
     <div className="switch-buttons">
-      <button type="button" onClick={tongleTheme} className="switch-button">
+      <button
+        type="button"
+        onClick={myToggleThemeLight}
+        className="switch-button"
+      >
         <Icon
           name="icon-sun"
           className={`switch-icon ${theme === "light" ? "white" : "dark"} ${
@@ -16,7 +29,11 @@ const SwitchThemeButtons = () => {
           }`}
         />
       </button>
-      <button type="button" onClick={tongleTheme} className="switch-button">
+      <button
+        type="button"
+        onClick={myToggleThemeDark}
+        className="switch-button"
+      >
         <Icon
           name="icon-moon"
           className={`switch-icon ${theme === "light" ? "accent" : "light"} ${
@@ -29,26 +46,3 @@ const SwitchThemeButtons = () => {
 };
 
 export default SwitchThemeButtons;
-
-{
-  /* <Icon
-          name="icon-sun"
-          className={`switch-icon ${
-            location.pathname !== "/home"
-              ? ""
-              : theme === "light"
-              ? "home white"
-              : "home dark"
-          }`}
-        />
-        <Icon
-        name="icon-moon"
-        className={`switch-icon ${
-          location.pathname !== "/home"
-            ? ""
-            : theme === "light"
-            ? "home accent"
-            : "home light"
-        }`}
-      /> */
-}
