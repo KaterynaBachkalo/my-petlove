a7155937c65b63a1c8fdb64c4c4db8a1e50a2102
KaterynaBachkalo <kozlovets88@gmail.com>
2025-05-08 13:15:19 +0300

    add buttons for publication

--- src/components/ProfileComponents/MyPet.tsx
+++ src/components/ProfileComponents/MyPet.tsx
@@ -1,9 +1,11 @@
 import Icon from "../ComponentsForDesign/Icon";
 import { IMyPet } from "../../types";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../redux/store";
 import { deletePet } from "../../redux/auth/operations";
 import { addToNotices, deleteFromNotices } from "../../redux/pet/operations";
+import { useEffect, useState } from "react";
+import { selectNotices } from "../../redux/pet/selectors";
 
 interface IProps {
   data: IMyPet;
@@ -12,19 +14,39 @@ interface IProps {
 const MyPet = ({ data }: IProps) => {
   const { sex, imgURL, title, name, birthday, species, _id } = data;
 
+  const [isCheck, setCheck] = useState(false);
+
+  const notices = useSelector(selectNotices);
+
   const dispatch = useDispatch<AppDispatch>();
 
   const deleteMyPet = () => {
-    if (_id) dispatch(deletePet(_id));
+    if (_id) {
+      dispatch(deletePet(_id));
+      dispatch(deleteFromNotices(_id));
+    }
   };
 
   const publicMyPet = () => {
-    if (_id) dispatch(addToNotices(_id));
+    if (!isCheck) {
+      if (_id) dispatch(addToNotices(_id));
+      setCheck(true);
+    }
   };
 
   const deleteFromPublicMyPet = () => {
-    if (_id) dispatch(deleteFromNotices(_id));
+    if (isCheck) {
+      if (_id) dispatch(deleteFromNotices(_id));
+      setCheck(false);
+    }
   };
+
+  useEffect(() => {
+    if (_id) {
+      const existPet = notices.find((pet) => pet._id === _id);
+      setCheck(!!existPet);
+    }
+  }, [notices, _id]);
 
   return (
     <div className="my-pet-border">
@@ -64,12 +86,28 @@ const MyPet = ({ data }: IProps) => {
       <div className="my-pet-delete" onClick={deleteMyPet}>
         <Icon className="icon-heart delete" name="delete" />
       </div>
-      <button type="button" onClick={publicMyPet}>
-        To public
-      </button>
-      <button type="button" onClick={deleteFromPublicMyPet}>
-        Delete from publication
-      </button>
+      <div className="my-pet-button-wrapper">
+        <button
+          type="button"
+          onClick={publicMyPet}
+          className="form-button submit to-publish"
+        >
+          {isCheck ? (
+            <>
+              Published <Icon name="icon-check" className="icon-check" />
+            </>
+          ) : (
+            "To publish"
+          )}
+        </button>
+        <button
+          type="button"
+          onClick={deleteFromPublicMyPet}
+          className="form-button back remove"
+        >
+          Remove from publication
+        </button>
+      </div>
     </div>
   );
 };
